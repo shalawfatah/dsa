d3423d6ed91b625c08f5865879e59ecdd8a34cb1
Shalaw Fatah <shalaw.fatah@gmail.com>
2026-04-20 10:30:54 +0300

    practice done for solution 2 of question 2

--- practice.js
+++ practice.js
@@ -1,8 +1,15 @@
-const s = "racecAr";
-function strToObj(str) {
-  for (let x of str.toLowerCase()) {
-  }
-  return obj;
-}
+const str_1 = "anagram";
+const str_2 = "gramana";
 
-console.log(strToObj(s));
+function is_valid_anagram(x, y) {
+  const obj = {};
+  for (let item of x) {
+    obj[item] = (obj[item] || 0) + 1;
+  }
+  for (let item of y) {
+    if (!obj[item]) return false;
+    obj[item]--;
+  }
+  return true;
+}
+console.log(is_valid_anagram(str_1, str_2));
