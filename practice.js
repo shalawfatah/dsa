e5a1a6fa9d63124d91076f5b5daaba8cc856a845
Shalaw Fatah <shalaw.fatah@gmail.com>
2026-04-20 11:23:59 +0300

    second solution

--- practice.js
+++ practice.js
@@ -6,9 +6,12 @@ function is_valid_anagram(x, y) {
   for (let item of x) {
     obj[item] = (obj[item] || 0) + 1;
   }
+  // HERE obj = {a = 3, n = 1, g = 1, r = 1, m = 1}
   for (let item of y) {
     if (!obj[item]) return false;
+    // IF THERE IS NO g,r,a,m,n THEN NO ANAGRAM
     obj[item]--;
+    // IF THERE IS g,r,a,m,n, DECREMENT VALUE
   }
   return true;
 }
