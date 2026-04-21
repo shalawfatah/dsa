b494e28c3fc7a6ebe6f73d99e6c3279f16563686
Shalaw Fatah <shalaw.fatah@gmail.com>
2026-04-21 10:28:51 +0300

    solution to two-sum

--- 3_two_sum.js
+++ 3_two_sum.js
@@ -6,5 +6,17 @@ You may assume that *every* input has exactly one pair of indices `i` and `j` th
 Return the answer with the smaller index first.
 */
 
-const nums = [3, 4, 5, 6];
+const nums = [8, 9, 10, 5, 3, 4];
+function two_sum(arr, target) {
+  const map = new Map();
+  for (let i = 0; i < arr.length; i++) {
+    const completion = target - arr[i];
+    if (map.has(completion)) {
+      return [map.get(completion), i];
+    }
+    map.set(arr[i], i);
+  }
+  return [];
+}
 const target = 7;
+console.log(two_sum(nums, 7));
