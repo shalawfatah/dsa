a4eb622d5db959228fa6a6a1eb339620ecb37780
Shalaw Fatah <shalaw.fatah@gmail.com>
2026-04-23 14:19:17 +0300

    5th issue solved

--- 5_top_k_frequent.js
+++ 5_top_k_frequent.js
@@ -7,6 +7,17 @@ You may return the output in **any order**.
 */
 
 const nums = [1, 2, 2, 3, 3, 3];
-const k = 2;
+const x = 2;
 
-function topKFrequent(arr) {}
+function topKFrequent(arr, k) {
+  const map = new Map();
+  for (let item of arr) {
+    map.set(item, (map.get(item) || 0) + 1);
+  }
+  return Array.from(map.entries())
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, k)
+    .map((pair) => pair[0]);
+}
+
+console.log(topKFrequent(nums, x));
