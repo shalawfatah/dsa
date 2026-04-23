3e95c2d1f6bd8cdd0eb06df652dcf98699525214
Shalaw Fatah <shalaw.fatah@gmail.com>
2026-04-23 14:48:41 +0300

    practice done

--- practice.js
+++ practice.js
@@ -9,6 +9,15 @@ You may return the output in **any order**.
 const nums = [1, 2, 2, 3, 3, 3];
 const x = 2;
 
-function topKFrequent(arr, k) {}
+function topKFrequent(arr, k) {
+  const map = new Map();
+  for (let item of arr) {
+    map.set(item, (map.get(item) || 0) + 1);
+  }
+  return Array.from(map.entries())
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, k)
+    .map((item) => item[0]);
+}
 
 console.log(topKFrequent(nums, x));
