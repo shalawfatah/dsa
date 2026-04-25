bfc325f39e621897bf9be7fd5b7e9cbea03543f1
Shalaw Fatah <shalaw.fatah@gmail.com>
2026-04-25 06:12:16 +0300

    function 7 added

--- 7_products_array_except_self.js
+++ 7_products_array_except_self.js
@@ -5,3 +5,8 @@ Each product is **guaranteed** to fit in a **32-bit** integer.
 
 Follow-up: Could you solve it in O(n)O(n) time without using the division operation?
 */
+
+const nums = [1, 2, 4, 6];
+const nums_2 = [-1, 0, 1, 2, 3];
+
+function product_except_self(nums) {}
