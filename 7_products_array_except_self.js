2c3cf1d306ac81e04c56a7c6bae4d5e21387c792
Shalaw Fatah <shalaw.fatah@gmail.com>
2026-04-25 12:17:04 +0300

    initial product

--- 7_products_array_except_self.js
+++ 7_products_array_except_self.js
@@ -9,7 +9,25 @@ Follow-up: Could you solve it in O(n)O(n) time without using the division operat
 const nums = [1, 2, 4, 6];
 const nums_2 = [-1, 0, 1, 2, 3];
 
-function product_except_self(nums) {}
+function product_except_self(nums) {
+  const n = nums.length;
+  const output = new Array(n).fill(1);
+
+  let leftProduct = 1;
+  for (let i = 0; i < n; i++) {
+    output[i] = leftProduct;
+    leftProduct *= nums[i];
+  }
+  console.log("left output ", output);
+
+  let rightProduct = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    output[i] *= rightProduct;
+    rightProduct *= nums[i];
+  }
+
+  return output;
+}
 
 console.log(product_except_self(nums));
 console.log(product_except_self(nums_2));
